da9bfb57786c59f8d235e04bda7e13cda283fcd3
huzaifa8883 <aw3540341@gmail.com>
2025-10-29 00:17:36 +0500

    Update Withdrawpin.jsx

--- src/components/Withdrawpin.jsx
+++ src/components/Withdrawpin.jsx
@@ -28,7 +28,7 @@ const WithdrawPage = () => {
 
     try {
       const response = await axios.put(
-        "https://casino2-production.up.railway.app/api/auth/update-withdraw-pin",
+        "https://ganto.work.gd/api/auth/update-withdraw-pin",
         {
           userId,
           newWithdrawPin,
